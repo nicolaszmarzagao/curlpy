8ee9d385892522323ec36f420b74f462ed396b8d
Nicolas Marzagao <nicolas.marzagao@gmail.com>
2026-05-21 17:13:37 -0300

    fix: refactor

--- src/curlpy/cli.py
+++ src/curlpy/cli.py
@@ -2,6 +2,15 @@ import argparse
 import socket
 import ssl
 from urllib.parse import urlparse
+from dataclasses import dataclass
+
+@dataclass
+class HttpRequest:
+    scheme: str
+    host: str
+    port: int
+    path: str
+    method: str
 
 def get_default_port(scheme):
     if scheme == "https": 
@@ -9,41 +18,67 @@ def get_default_port(scheme):
     else: 
         return 80
 
-def main() -> None:
+def remove_header(output):
+
+    return output
+
+def setup_arguments():
     parser = argparse.ArgumentParser(
         prog="curlpy",
         description="A implementation of curl with Python3",
     )
-
-    parser.add_argument("url", type=str, help="URL curl will connect to.")
+    parser.add_argument(
+        "url", type=str, 
+        help="URL curl will connect to."
+    )
     parser.add_argument(
         "-p", "--port", 
         type=int, help="Port that curl will connect to."
     )
+    parser.add_argument(
+        "-i", "--include-headers",
+        action="store_true", help="Keeps HTTP header in output."
+    )
 
+    return parser
+
+def parse_arguments(parser):
     args = parser.parse_args()
     parsed_url = urlparse(args.url)
 
     if args.port is not None:
         port = args.port
     elif parsed_url.port is not None:
-        port = parsed_url.port
+        port = parsed_url
     else:
         port = get_default_port(parsed_url.scheme)
 
-    s = socket.create_connection((parsed_url.hostname, port))
-    if parsed_url.scheme == "https":
-        context = ssl.create_default_context()
-        s = context.wrap_socket(s, server_hostname=parsed_url.hostname)
+    request = HttpRequest(
+        scheme=parsed_url.scheme,
+        host=parsed_url.hostname,
+        port=port,
+        path=parsed_url.path,
+        method="GET"
+    )
 
-    get_request = (
-        f"GET {parsed_url.path} HTTP/1.1\r\n"
-        f"Host: {parsed_url.hostname}\r\n"
+    return request
+
+def make_request(request):
+    s = socket.create_connection((request.host, request.port))
+
+    if request.scheme == "https":
+        context = ssl.create_default_context()
+        s = context.wrap_socket(s, server_hostname=request.host)
+
+    r = (
+        f"{request.method} {request.path} HTTP/1.1\r\n"
+        f"Host: {request.host}\r\n"
         f"User-Agent: curlpy/0.1\r\n"
         f"Connection: close\r\n"
         f"\r\n"
     )
-    s.sendall(get_request.encode())
+
+    s.sendall(r.encode())
 
     response = b""
     while True:
@@ -54,6 +89,17 @@ def main() -> None:
 
     s.close()
 
-    print(response.decode(errors="replace").split("\r\n\r\n")[1])
+    return response
+
+
+def main() -> None:
+    parser     = setup_arguments()
+    request    = parse_arguments(parser)
+    response   = make_request(request).decode(errors="replace")
+
+    if parser.parse_args().include_headers is False: 
+        print(response.split("\r\n\r\n")[1])
+    else:
+        print(response)
 
 
