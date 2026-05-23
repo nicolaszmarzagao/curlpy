31b0e33aa4a99995259155db69784a70a5a9ffb1
Nicolas Marzagao <nicolas.marzagao@gmail.com>
2026-05-23 20:13:53 -0300

    request.py

--- src/curlpy/request.py
+++ src/curlpy/request.py
@@ -2,30 +2,45 @@ import socket
 import ssl
 
 
-def make_request(request):
-    s = socket.create_connection((request.host, request.port))
-
-    if request.scheme == "https":
+def wrap_socket(socket, scheme, host):
+    if scheme == "https":
         context = ssl.create_default_context()
-        s = context.wrap_socket(s, server_hostname=request.host)
+        return context.wrap_socket(socket, server_hostname=host)
+    return socket
 
-    r = (
-        f"{request.method} {request.path} HTTP/1.1\r\n"
-        f"Host: {request.host}\r\n"
+
+# add agent later
+def create_request(method, path, host):
+    return (
+        f"{method} {path} HTTP/1.1\r\n"
+        f"Host: {host}\r\n"
         f"User-Agent: curlpy/0.1\r\n"
         f"Connection: close\r\n"
         f"\r\n"
     )
 
-    s.sendall(r.encode())
 
+def receive_response(socket):
     response = b""
     while True:
-        chunk = s.recv(4096)
+        chunk = socket.recv(4096)
         if not chunk:
             break
         response += chunk
 
-    s.close()
+    return response
+
+
+def make_request(request):
+    socket_con = wrap_socket(
+        socket.create_connection((request.host, request.port)),
+        request.scheme,
+        request.host,
+    )
+    request = create_request(request.method, request.path, request.host)
+    socket_con.sendall(request.encode())
+    response = receive_response(socket_con)
+
+    socket_con.close()
 
     return response
